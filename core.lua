716173cd78d03ab6193b80eea9516ea46a03fefc
mtimo101289@gmail.com <mtimo101289@gmail.com>
2020-01-03 20:23:19 +0100

    checkpoint

--- core.lua
+++ core.lua
@@ -2,6 +2,10 @@
 local roster = {};
 local location = nil;
 local minLevel = nil;
+local roles = false;
+local heal = nil;
+local dps = nil;
+local tank = nil;
 
 -- Util methods
 local splitStr = function (inputstr, sep)
@@ -17,7 +21,7 @@ local updateRoster = function()
 
     for i = 1, numMembers, 1 do
         local nameWithServer, rank, rankIndex, level = GetGuildRosterInfo(i);
-        local name = splitStr(nameWithServer, "-")[1]
+        local name = splitStr(nameWithServer, "-")[1];
         roster[name] = level;
     end
 end
@@ -32,6 +36,29 @@ local convertIfNeeded = function()
 	end
 end
 
+local extractRoles = function(rolesArg)
+    if rolesArg == nil then return end
+
+    local rolesPayload = splitStr(rolesArg, "/");
+    heal = tonumber(rolesPayload[1]);
+    dps = tonumber(rolesPayload[2]);
+    tank = tonumber(rolesPayload[3]);
+
+    if heal ~= nil and dps ~= nil and tank ~= nil then
+        roles = true;
+    end
+end
+
+
+local reset = function()
+    location = nil;
+    minLevel = nil;
+    roles = false;
+    heal = nil;
+    dps = nil;
+    tank = nil;
+end
+
 -- Declare frame
 local f = CreateFrame("Frame");
 -- Declare slash command
@@ -42,12 +69,12 @@ SlashCmdList["INVITOMATIC"] = function(inp)
 
     if payload[1] == nil then
 		if location ~= nil then
-			--f:UnregisterEvent("CHAT_MSG_WHISPER");
-            f:UnregisterEvent("CHAT_MSG_GUILD");
-			f:SetScript("OnEvent", nil);
-            SendChatMessage("INVITOMATIC: disabled auto invites for " .. location, "GUILD");
-            location = nil;
-            minLevel = nil;
+			f:UnregisterEvent("CHAT_MSG_WHISPER");
+            --f:UnregisterEvent("CHAT_MSG_GUILD");
+            f:SetScript("OnEvent", nil);
+            print("INVITOMATIC: disabled auto invites for " .. location);
+            --SendChatMessage("INVITOMATIC: disabled auto invites for " .. location, "GUILD");
+            reset();
         end
         do return end
     end
@@ -63,9 +90,10 @@ SlashCmdList["INVITOMATIC"] = function(inp)
         minLvl = 60;
     end
 
+    extractRoles(payload[3]);
 
-	--f:RegisterEvent("CHAT_MSG_WHISPER");
-    f:RegisterEvent("CHAT_MSG_GUILD");
+	f:RegisterEvent("CHAT_MSG_WHISPER");
+    --f:RegisterEvent("CHAT_MSG_GUILD");
     f:SetScript("OnEvent", function(self, event, msg, author, language, lineId, senderGUID)
 		local msgLower = string.lower(msg);
 		if (msgLower == "inv" or msgLower == "invite") then
@@ -84,8 +112,18 @@ SlashCmdList["INVITOMATIC"] = function(inp)
     end)
 
     local affix = ". ";
+    local rolesAffix = "";
+    local minLvlAffix = "";
+    local invAffix = "Type inv for auto invite";
+
+    if roles then
+        rolesAffix = "Need " .. heal .. " heal, " .. dps .. " dps and " .. tank .. " tank. ";
+        invAffix = "Type inv heal/dps/tank (PICK ONE) for auto invite";
+    end
+
     if minLvl then
-        affix = affix .. "Min level " .. minLvl .. ". ";
-	end
-    SendChatMessage("INVITOMATIC: LFM for " .. location .. affix .. "Type inv for auto invite", "GUILD");
+        minLvlAffix = "Min level " .. minLvl .. ". ";
+    end
+    print("INVITOMATIC: LFM for " .. location .. ". " .. rolesAffix .. minLvlAffix .. invAffix);
+    --SendChatMessage("INVITOMATIC: LFM for " .. location .. affix .. "Type inv for auto invite", "GUILD");
 end
